b3d24d728fc223372e4522fef7a35a09698974b4
PukkaPad <marianasouza.rj@gmail.com>
2017-09-18 16:58:19 +0100

    API: selecting columns from boro

--- api_development/server.py
+++ api_development/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api,  fields, reqparse
 from sqlalchemy import create_engine
 from json import dumps
 from flask_jsonpify import jsonify
@@ -13,6 +13,14 @@ class DBN(Resource):
         conn = db_connect.connect() # connect to database
         query = conn.execute("select * from AllData") # This line performs query and returns json result
         return {'DBN': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is DBN
+
+
+class Boro(Resource):
+    def get(self, boro_name):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select school_name, overview_paragraph, program_highlights, SAT_score from AllData where boro ='%s'" %boro_name)
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
 
 class AllCorrelation(Resource):
     def get(self):
@@ -28,9 +36,11 @@ class Correlation(Resource):
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
-api.add_resource(DBN, '/DBN') # Route_1
-api.add_resource(AllCorrelation, '/Correlation') # Route_2
-api.add_resource(Correlation, '/SATCorrelation') # Route_2
+
+api.add_resource(DBN, '/v1.0/DBN') # Route_1
+api.add_resource(AllCorrelation, '/v1.0/Correlation')
+api.add_resource(Correlation, '/v1.0/SATCorrelation')
+api.add_resource(Boro, '/v1.0/boro/<string:boro_name>')
 
 if __name__ == '__main__':
      app.run(port='5002')
